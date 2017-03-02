ffa3bbc3f686445d3855675d4474a04cb2132a72
Sumit1009 <sumit.sharma@tothenew.com>
2017-03-02 19:22:54 +0530

    Sumit-Sharma Exercise

--- Exercise/exercise4_1.js
+++ Exercise/exercise4_1.js
@@ -4,3 +4,5 @@
 var util=require('./exercise4_2.js');
 //Enter date of birth in MM/DD/YYY Format
 console.log(util.ageCalculator('09/10/1995'))
+console.log(util.ageCalculator('01/11/2005'))
+console.log(util.ageCalculator('09/10/2018'))
